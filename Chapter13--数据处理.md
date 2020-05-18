4bb4dcadf58ddbfe9d1322e96c70b238908be5d3
duzexing <duzexing@outlook.com>
2020-05-18 09:20:27 +0800

    5/18

--- Chapter13--数据处理.md
+++ Chapter13--数据处理.md
@@ -17,3 +17,39 @@
 ![image-20200514171654316](C:\Users\duzex\AppData\Roaming\Typora\typora-user-images\image-20200514171654316.png)
 
 ![image-20200514172805140](C:\Users\duzex\AppData\Roaming\Typora\typora-user-images\image-20200514172805140.png)
+
+![image-20200515084230551](C:\Users\duzex\AppData\Roaming\Typora\typora-user-images\image-20200515084230551.png)
+
+![image-20200515084308923](C:\Users\duzex\AppData\Roaming\Typora\typora-user-images\image-20200515084308923.png)
+
+![image-20200515084907371](C:\Users\duzex\AppData\Roaming\Typora\typora-user-images\image-20200515084907371.png)
+
+![image-20200515090306284](C:\Users\duzex\AppData\Roaming\Typora\typora-user-images\image-20200515090306284.png)
+
+## torchvision集成数据集
+
+![image-20200515090325913](C:\Users\duzex\AppData\Roaming\Typora\typora-user-images\image-20200515090325913.png)
+
+![image-20200515090707658](C:\Users\duzex\AppData\Roaming\Typora\typora-user-images\image-20200515090707658.png)
+
+![image-20200515091119487](C:\Users\duzex\AppData\Roaming\Typora\typora-user-images\image-20200515091119487.png)
+
+![image-20200515093322344](C:\Users\duzex\AppData\Roaming\Typora\typora-user-images\image-20200515093322344.png)
+
+![image-20200515093359379](C:\Users\duzex\AppData\Roaming\Typora\typora-user-images\image-20200515093359379.png)
+
+![image-20200515094744299](C:\Users\duzex\AppData\Roaming\Typora\typora-user-images\image-20200515094744299.png)
+
+## 加载batch--DataLoader
+
+![image-20200515095157416](C:\Users\duzex\AppData\Roaming\Typora\typora-user-images\image-20200515095157416.png)
+
+![image-20200515095429081](C:\Users\duzex\AppData\Roaming\Typora\typora-user-images\image-20200515095429081.png)
+
+![image-20200515095558057](C:\Users\duzex\AppData\Roaming\Typora\typora-user-images\image-20200515095558057.png)
+
+## 数据加载时存在的样本损坏问题
+
+![image-20200515100033037](C:\Users\duzex\AppData\Roaming\Typora\typora-user-images\image-20200515100033037.png)
+
+![image-20200515100537451](C:\Users\duzex\AppData\Roaming\Typora\typora-user-images\image-20200515100537451.png)
--- Chapter13-code.py
+++ Chapter13-code.py
@@ -3,24 +3,54 @@ from PIL import Image
 import numpy as np
 from torch.utils import data
 import os
+from torchvision import transforms
+import PIL
+
+transform = transforms.Compose([transforms.Resize(224),
+                                transforms.CenterCrop(224),
+                                transforms.ToTensor(),
+                                transforms.Normalize(mean=[0.5, 0.5, 0.5], std = [0.5, 0.5, 0.5])
+                                ])
 
 class DogCat(data.Dataset):
-    def __init__(self, root):
+    def __init__(self, root, transforms = None):
         imgs = os.listdir(root) #图像的绝对路径
         self.imgs = [os.path.join(root, img) for img in imgs]
+        self.transform = transforms
 
     def __getitem__(self, index):
         img_path = self.imgs[index]
         label = 1 if 'dog' in img_path.split('/')[-1] else 0
-        pil_img = Image.open(img_path)
-        array = np.array(pil_img)
-        data = torch.from_numpy(array)
+        data = Image.open(img_path)
+        #array = np.array(pil_img)
+        #data = torch.from_numpy(array)
+        if self.transform:
+            data = self.transform(data)
         return data, label
 
     def __len__(self):
         return len(self.imgs)
-
-dataset = DogCat(r'./images/')
+'''
+dataset = DogCat(r'./images/', transforms = transform)
 img, label = dataset[0]
 for img, label in dataset:
     print(img.size(), img.float().mean(), label)
+'''
+class new_DogCat(DogCat):
+    def __getitem__(self, index):
+        try:
+            return super().__getitem__(index) #注意return
+        except PIL.UnidentifiedImageError:
+            return None, None  
+
+dataset = new_DogCat('imageswrong', transform)
+from torch.utils.data.dataloader import default_collate #导入默认的拼接方式
+
+def my_collate_fn(batch):
+    # 过滤为None的数据
+    batch = list(filter(lambda x:x[0] is not None, batch))
+    return default_collate(batch)
+
+dataloader = data.DataLoader(dataset, 3, collate_fn=my_collate_fn)
+for batch_data, batch_labels in dataloader:
+    print(batch_data.size(), batch_labels.size())
